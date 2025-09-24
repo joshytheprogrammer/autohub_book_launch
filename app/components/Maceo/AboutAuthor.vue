8b24c6198f5152bb1ff643b3371fcce6cfb85683
Joshua <jurassicjoshua@proton.me>
2025-09-24 21:41:25 +0100

    made changes: ''

--- app/components/Maceo/AboutAuthor.vue
+++ app/components/Maceo/AboutAuthor.vue
@@ -18,7 +18,7 @@
               <div class="aspect-[4/5] bg-gradient-to-br from-green-100 to-indigo-100 rounded-2xl overflow-hidden">
                 <!-- Placeholder for author image -->
                 <img 
-                  src="/jib.png" 
+                  src="https://res.cloudinary.com/dsgvwxygr/image/upload/v1758746446/jib_wduy54.png" 
                   alt="Chika Joel (J-ib)" 
                   class="w-full h-full object-cover"
                   loading="lazy"
